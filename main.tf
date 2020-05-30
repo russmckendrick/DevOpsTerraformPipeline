f871ff0b9444f291a63cfa1e37951a1e2b829fa8
Russ Mckendrick <russ@mckendrick.io>
2020-05-30 16:04:41 +0100

    Update
    - fixing wrong variable name

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@
 # What is the minimum version of Terraform we need?
 terraform {
   required_version = ">= 0.12.0"
+  backend "azurerm"
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/
@@ -11,4 +12,3 @@ provider "azurerm" {
   version = "=2.0"
   features {}
 }
-
