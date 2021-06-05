55e361efadbe7635bbdfdca15c5fd48af81a472d
Russ Mckendrick <russ@mckendrick.io>
2021-06-05 14:55:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,10 @@ resource "azurerm_resource_group" "resource_group" {
   location = "uksouth"
   tags     = merge(var.default_tags, map("type", "resource"))
 }
+
+resource "azurerm_management_lock" "resource-group-lock" {
+  name       = "resource-group-lock"
+  scope      = azurerm_resource_group.resource_group.id
+  lock_level = "CanNotDelete"
+  notes      = "This Resource Group can not be deleted"
+}
