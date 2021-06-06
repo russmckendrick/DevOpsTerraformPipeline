06f4f81a01e86263f68e3cfd087f658dacd6a966
Russ Mckendrick <russ@mckendrick.io>
2021-06-06 13:46:29 +0100

    Update

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ terraform {
   required_version = ">= 0.15.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"  # https://registry.terraform.io/providers/hashicorp/azurerm/latest
       version = "=2.46.0"
     }
     azurecaf = {
-      source = "aztfmod/azurecaf"
+      source  = "aztfmod/azurecaf" # https://registry.terraform.io/providers/aztfmod/azurecaf/latest
       version = "1.2.3"
     }
   }
@@ -30,18 +30,16 @@ provider "azurecaf" {
 ######################################################################################################
 
 resource "azurecaf_name" "rg_example" {
-  name            = "demogroup"
-    resource_type   = "azurerm_resource_group"
-    prefixes        = ["a", "b"]
-    suffixes        = ["y", "z"]
-    random_length   = 5
-    clean_input     = true
+  name          = "demogroup"
+  resource_type = "azurerm_resource_group"
+  prefixes      = ["dev"]
+  clean_input   = true
 }
 
 resource "azurerm_resource_group" "resource_group" {
   name     = azurecaf_name.rg_example.result
   location = "uksouth"
-  tags     = merge(var.default_tags, tomap({"type" = "resource"}))
+  tags     = merge(var.default_tags, tomap({ "type" = "resource" }))
 }
 
 resource "azurerm_management_lock" "resource-group-lock" {
