27098aca815900ee87776beb4981065a88569c81
Russ Mckendrick <russ@mckendrick.io>
2021-08-22 17:45:37 +0100

    Update -

--- variables.tf
+++ variables.tf
@@ -7,3 +7,15 @@ variable "default_tags" {
     deployed_using = "terraform"
   }
 }
+
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "sku" {
+}
